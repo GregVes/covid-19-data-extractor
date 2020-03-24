0961c217cf5529ddecf30ece9833f2a0b28eb2ae
GregVes <vespasiengregory@gmail.com>
2020-03-24 17:42:36 +0100

    tests/helpers

---
+++ __tests_/helpers.spec.js
@@ -0,0 +1,20 @@
+const Helpers = require("../helpers");
+
+
+describe("Helpers function", () => {
+    beforeEach(() => {
+    });
+    test("it formats a date to fit COVID-19 CSV file format", () => {
+        expect(Helpers.format("2020-03-10")).toEqual("03-10-2020");
+    });
+    test("it turns a CSV row into a ReportDto", () => {
+        const row = ["", "", "", "France", "2020-03-20", "", "", 100, 100]
+        const expected = {
+            country: row[3],
+            reportDate: row[4],
+            cases: row[7],
+            dead: row[8]
+        }
+        expect(Helpers.toDto(row)).toEqual(expected);
+    });
+});
